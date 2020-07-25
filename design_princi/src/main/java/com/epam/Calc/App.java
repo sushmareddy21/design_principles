a6a161fc6622e8468f917ed6475f04b9385ead06
sushma reddy <59745323+sushmareddy21@users.noreply.github.com>
2020-07-25 14:29:05 +0530

    Add files via upload
    design principles

---
+++ design_princi/src/main/java/com/epam/Calc/App.java
@@ -0,0 +1,25 @@
+package com.epam.Calc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+	public int addition(int a,int b)
+	{
+		return a+b;
+	}
+	public  int subtraction(int a,int b)
+	{
+		return a-b;
+	}
+	public int multiplication(int a,int b)
+	{
+		return a*b;
+	}
+	public int division(int a,int b)
+	{
+		return a/b;
+	}
+}
